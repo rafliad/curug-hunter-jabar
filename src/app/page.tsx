584ac00601b210add22795b3a2c9246d6197cfd6
rafliad <radenistri@gmail.com>
2025-07-28 22:01:51 +0700

    chore: migrate package manager from npm to pnpm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import prisma from "@/lib/prisma";
-import Link from "next/link";
-import SafeImage from "@/components/SafeImage";
 import CurugBrowser from "@/components/CurugBrowser";
 
 export default async function HomePage() {
-  const allCurug = await prisma.curug.findMany({
+  const allCurugData = await prisma.curug.findMany({
     orderBy: { name: "asc" },
   });
+
+  const allCurug = JSON.parse(JSON.stringify(allCurugData));
 
   return (
     <div className="max-w-7xl mx-auto p-4 sm:p-8">
