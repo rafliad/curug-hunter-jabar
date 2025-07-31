b7404667fb25a14e42b3487f9bf33edd5eb13a1d
rafliad <radenistri@gmail.com>
2025-07-31 13:17:30 +0700

    feat(ui): implement final navbar and footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers"; // Import providers
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,8 +23,8 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           <main>{children}</main>
+          <Footer />
         </Providers>{" "}
-        {/* Bungkus children dengan Providers */}
       </body>
     </html>
   );
